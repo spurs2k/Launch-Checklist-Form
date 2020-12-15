61ea785ebd106337c9c75721d5ce5f8a04ee1b16
James Spurlin <spurlinjames@gmail.com>
2020-12-14 21:55:54 -0600

    Final commit, maybe

--- script.js
+++ script.js
@@ -1,13 +1,96 @@
-// Write your JavaScript code here!
+//Event listener to fetch planetary json data.  .then used to handle promises
+window.addEventListener("load", () => {
+   
+   //Initializing ID items
+   let pilotNameInput = document.querySelector("input[name=pilotName]");
+   let copilotNameInput = document.querySelector("input[name=copilotName]");
+   let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+   let cargoMassInput = document.querySelector("input[name=cargoMass]");
+   let pilotStatus = document.getElementById("pilotStatus");
+   let copilotStatus = document.getElementById("copilotStatus");
+   let fuelStatus = document.getElementById("fuelStatus");
+   let cargoStatus = document.getElementById("cargoStatus");
+   let missionTarget = document.getElementById("missionTarget");
+   let faultyItems = document.getElementById("faultyItems");
+   let launchStatus = document.getElementById("launchStatus");
+   let formSubmit = document.getElementById("formSubmit");
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+
+   fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(res) {
+         res.json().then(function(json) {
+         missionTarget.innerHTML = `
+            <h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${json[0].name}</li>
+               <li>Diameter: ${json[0].diameter}</li>
+               <li>Star: ${json[0].star}</li>
+               <li>Distance from Earth: ${json[0].distance}</li>
+               <li>Number of Moons: ${json[0].moons}</li>
+         </ol>
+         <img src="${json[0].image}"></img>
+         `; 
+         });
+   });
+
+   //Event listener for the submit button when clicked
+   formSubmit.addEventListener("click", function(event){
+   
+      if (pilotNameInput.value === "" || 
+         copilotNameInput.value === "" ||
+         fuelLevelInput.value === "" ||
+         cargoMassInput.value === ""
+      ) {
+         alert("All fields are required.");
+         event.preventDefault();
+      } else {
+       pilotStatus.innerText = `Pilot ${pilotNameInput.value} is ready for launch!`;
+       copilotStatus.innerText = `Co-pilot ${copilotNameInput.value} is ready for launch!`;
+       systemsCheck();
+      }
+
+    //Check to make sure Cargo and Fuel Level are numbers, alert if not.
+    function systemsCheck(){
+      if (isNaN(cargoMassInput.value) || isNaN(fuelLevelInput.value)){
+         window.alert("Make sure to enter valid information for each field!")
+         event.preventDefault();
+         } else {
+            fuelCheck();
+         }
+      }
+
+      //Functions to check the fuel and cargo level
+      function fuelCheck(){
+         if (fuelLevelInput.value < 10000){
+            fuelStatus.innerText = "Fuel level is too low to launch."
+            shuttleNotReady();
+         } else {
+            cargoCheck();
+         }
+      }
+
+      function cargoCheck(){
+         if (cargoMassInput.value > 10000){
+            cargoStatus.innerText = "Mass is too high for the shuttle launch."
+            shuttleNotReady();
+         } else {
+            shuttleReady();
+         }
+      }
+
+      function shuttleReady(){
+         launchStatus.style.color = "green";
+         launchStatus.innerText = "Shuttle is ready for launch";
+         faultyItems.style.visibility = "visible";
+         event.preventDefault();
+      }
+
+      function shuttleNotReady(){
+         launchStatus.style.color = "red";
+         launchStatus.innerText = "Shuttle not ready for launch."
+         faultyItems.style.visibility = "visible";
+         event.preventDefault();
+      }
+
+   });
+
+});
